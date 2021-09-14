fb67313b7f77cfcf4377a13873ff6fa6229a46fa
Obada <obadamoustafa@gmail.com>
2021-09-14 19:33:30 +0200

    all is perfect

--- script/manipulation/show-errors.js
+++ script/manipulation/show-errors.js
@@ -1,4 +1,4 @@
-import { locationDiv, locationField, moreDetailsSection } from "../constants.js";
+import { dateDiv, datePicker, locationDiv, locationField, moreDetailsSection } from "../constants.js";
 
 export function invalidCityName ( text ) {
     if (!document.querySelector('#invalid-city-error')) {
@@ -15,6 +15,24 @@ export function clearInvalidCityNameError(){
     if (document.querySelector('#invalid-city-error')) {
         document.querySelector('#invalid-city-error').remove();
         locationField.removeEventListener('input', clearInvalidCityNameError);
+    }
+}
+
+export function mustChooseDate ( text ) {
+    if (!document.querySelector('#must-choose-date')) {
+        const chooseDate = document.createElement('h5');
+        chooseDate.id = 'must-choose-date'
+        chooseDate.textContent = text;
+        dateDiv.appendChild(chooseDate);
+
+        datePicker.addEventListener('input', clearMustChooseDateError);
+    }
+}
+
+export function clearMustChooseDateError(){
+    if (document.querySelector('#must-choose-date')) {
+        document.querySelector('#must-choose-date').remove();
+        datePicker.removeEventListener('input', clearMustChooseDateError);
     }
 }
 
