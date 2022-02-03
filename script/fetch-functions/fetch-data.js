3e80b0b5adc8b50481aaaf6165d2ec59dd139bb3
ObadaElSharbatly <obadamoustafa@gmail.com>
2022-02-03 12:54:47 +0100

    removed postNl logos and fixed the height issue

--- script/fetch-functions/fetch-data.js
+++ script/fetch-functions/fetch-data.js
@@ -1,101 +1,91 @@
-import { datePicker, locationField, moreDetailsSection, timePicker } from "../constants.js";
+import {
+  datePicker,
+  locationField,
+  moreDetailsSection,
+  timePicker,
+} from "../constants.js";
 import { stopSearchButton } from "../manipulation/change-search-button.js";
 import { currentDate } from "../manipulation/restrict-dates.js";
-import { connectionError, invalidCityName, mustChooseDate } from "../manipulation/show-errors.js";
+import {
+  connectionError,
+  invalidCityName,
+  mustChooseDate,
+} from "../manipulation/show-errors.js";
 import { showGeneralInformation } from "../manipulation/view-weather-information.js";
 
 /* we need this to be global variable */
 export let searchMethod;
 
 function creatTheEndpointUrl() {
-    // Select the information entered by user
-    const locationValue = locationField.value;
-    const dateValue     = datePicker.value;
-    const timeValue     = timePicker.value;
-    
-    
-    const APIKey     = 'key=b458fd088f5b42c082691547210409';
-    const url        = `https://api.weatherapi.com/v1/`;
-    const locationPa = `&q=${locationValue}`;
-    const date       = `&dt=${dateValue}`
-    const time       = `&hour=${timeValue}`
+  // Select the information entered by user
+  const locationValue = locationField.value;
+  const dateValue = datePicker.value;
+  const timeValue = timePicker.value;
 
-    if(locationValue !== '' && dateValue === '' && timeValue === ''){
+  const APIKey = "key=b458fd088f5b42c082691547210409";
+  const url = `https://api.weatherapi.com/v1/`;
+  const locationPa = `&q=${locationValue}`;
+  const date = `&dt=${dateValue}`;
+  const time = `&hour=${timeValue}`;
 
-        searchMethod = 'current'
-        return `${url}${searchMethod}.json?${APIKey}${locationPa}`;
-
-    } 
-    else if (locationValue !== '' && dateValue >= currentDate) {
-
-        searchMethod = 'forecast'
-        if (timeValue === "") {
-            return `${url}${searchMethod}.json?${APIKey}${locationPa}${date}`;
-        }
-        else {  
-            return `${url}${searchMethod}.json?${APIKey}${locationPa}${date}${time}`;
-        }
-
-    } 
-    else if (locationValue !== '' && dateValue < currentDate){
-
-        searchMethod = 'history'
-        if (timeValue === "") {
-            return `${url}${searchMethod}.json?${APIKey}${locationPa}${date}`;
-        } 
-        else {  
-            return `${url}${searchMethod}.json?${APIKey}${locationPa}${date}${time}`;
-        }
-    } 
-    else {
-        invalidCityName('This is a required field');
+  if (locationValue !== "" && dateValue === "" && timeValue === "") {
+    searchMethod = "current";
+    return `${url}${searchMethod}.json?${APIKey}${locationPa}`;
+  } else if (locationValue !== "" && dateValue >= currentDate) {
+    searchMethod = "forecast";
+    if (timeValue === "") {
+      return `${url}${searchMethod}.json?${APIKey}${locationPa}${date}`;
+    } else {
+      return `${url}${searchMethod}.json?${APIKey}${locationPa}${date}${time}`;
     }
+  } else if (locationValue !== "" && dateValue < currentDate) {
+    searchMethod = "history";
+    if (timeValue === "") {
+      return `${url}${searchMethod}.json?${APIKey}${locationPa}${date}`;
+    } else {
+      return `${url}${searchMethod}.json?${APIKey}${locationPa}${date}${time}`;
+    }
+  } else {
+    invalidCityName("This is a required field");
+  }
 }
 
-export function fetchRightData(){
+export function fetchRightData() {
+  const endPointUrl = creatTheEndpointUrl();
 
-    const endPointUrl = creatTheEndpointUrl();
-    console.log(endPointUrl)
-
-    fetch(endPointUrl)
-    .then((receivedData)=>{
-        return receivedData.json();
+  fetch(endPointUrl)
+    .then((receivedData) => {
+      return receivedData.json();
     })
     .then((jsonWeatherData) => {
-        if (jsonWeatherData.error){
-            console.log(jsonWeatherData.error);
-            throw new Error(jsonWeatherData.error.message);
-        }
+      if (jsonWeatherData.error) {
+        throw new Error(jsonWeatherData.error.message);
+      }
 
-        console.log('json data success', jsonWeatherData)
-        // this function do the suitable behavior with the jason data.
-        showGeneralInformation(jsonWeatherData);
-        
+      // this function do the suitable behavior with the jason data.
+      showGeneralInformation(jsonWeatherData);
     })
 
-    .catch((error)=>{
-
-        // show error msg and reload button
-        if (error.message == 'Failed to fetch') {
-            console.log('the error is', error.message)
-            if (moreDetailsSection.classList.contains('hide')) {
-                moreDetailsSection.classList.remove('hide')
-            }
-            connectionError();
-        } 
-        else if (error.message === 'No matching location found.'){
-            console.log(error);
-            invalidCityName(error.message);
+    .catch((error) => {
+      // show error msg and reload button
+      if (error.message == "Failed to fetch") {
+        console.log("the error is", error.message);
+        if (moreDetailsSection.classList.contains("hide")) {
+          moreDetailsSection.classList.remove("hide");
         }
-        else if (error.message === 'dt or end_dt parameter should be in yyyy-MM-dd format and on or after 1st Jan, 2010 (2010-01-01).') {
-            mustChooseDate('Please choose a date')
-        } 
-        else {
-            console.log('error', error)
-            console.log('error msg', error.message)
-            throw error;
-            
-        }
-        
-    })
+        connectionError();
+      } else if (error.message === "No matching location found.") {
+        console.log(error);
+        invalidCityName(error.message);
+      } else if (
+        error.message ===
+        "dt or end_dt parameter should be in yyyy-MM-dd format and on or after 1st Jan, 2010 (2010-01-01)."
+      ) {
+        mustChooseDate("Please choose a date");
+      } else {
+        console.log("error", error);
+        console.log("error msg", error.message);
+        throw error;
+      }
+    });
 }
--- script/manipulation/view-weather-information.js
+++ script/manipulation/view-weather-information.js
@@ -1,123 +1,150 @@
-import { cityNameInfo, clockInfo, datePicker, dayInfo, generalInfoSection, temperatureDegree, tempEL, timePicker, weatherConditionInfo, weatherConditionLogo, temperatureType, locationIcon, locationField } from "../constants.js";
+import {
+  cityNameInfo,
+  clockInfo,
+  datePicker,
+  dayInfo,
+  generalInfoSection,
+  temperatureDegree,
+  tempEL,
+  timePicker,
+  weatherConditionInfo,
+  weatherConditionLogo,
+  temperatureType,
+  locationIcon,
+  locationField,
+} from "../constants.js";
 import { searchMethod } from "../fetch-functions/fetch-data.js";
 import { changeTempType } from "../Helper-functions/change-temp-type.js";
 import { showDayFormat, showRightTime } from "../Helper-functions/date.js";
-import { setWeatherDetails, setWeatherDetailsForCurrentLocation } from "./show-details-of-weather.js";
+import {
+  setWeatherDetails,
+  setWeatherDetailsForCurrentLocation,
+} from "./show-details-of-weather.js";
 
 let tempELHasEvenListener = false;
 /* variable which will use to save the function with params inside. */
 let changeTemperature;
 
-export function showGeneralInformation(jsonData){
-    generalInfoSection.classList.remove('hide')
-    cityNameInfo.textContent = `${jsonData.location.name} - ${jsonData.location.region} - ${jsonData.location.country}`;
-    temperatureType. textContent = 'C';
+export function showGeneralInformation(jsonData) {
+  generalInfoSection.classList.remove("hide");
+  cityNameInfo.textContent = `${jsonData.location.name} - ${jsonData.location.region} - ${jsonData.location.country}`;
+  temperatureType.textContent = "C";
 
-    if (tempELHasEvenListener) {
-        tempEL.removeEventListener('click', changeTemperature);
-    }
+  if (tempELHasEvenListener) {
+    tempEL.removeEventListener("click", changeTemperature);
+  }
 
-    if (clockInfo.classList.contains('hide')) {
-        clockInfo.classList.remove('hide');
-    }
+  if (clockInfo.classList.contains("hide")) {
+    clockInfo.classList.remove("hide");
+  }
 
-    /* Show information in case of current weather data request */
-    if(searchMethod === 'current'){
-
-        weatherConditionInfo.textContent = jsonData.current.condition.text;
-        weatherConditionLogo.src         = jsonData.current.condition.icon;
-        dayInfo.innerHTML                = showDayFormat(jsonData.location.localtime);
-        clockInfo.textContent            = `${showRightTime(jsonData.location.localtime)}`;
-        temperatureDegree.textContent    = Math.round(jsonData.current.feelslike_c);
-
-        // change the temp type
-        changeTemperature = () => 
-        changeTempType(jsonData.current.feelslike_f, jsonData.current.feelslike_c);
-        tempEL.addEventListener('click',changeTemperature);
-        tempELHasEvenListener = true;
-
-        /* show more details */
-        setWeatherDetails(jsonData.current);
-        
-    } 
-
-
-    /* Show information in case of forecast weather data request */
-    else if (searchMethod === 'forecast' || searchMethod === 'history') {
-        dayInfo.innerHTML = showDayFormat(datePicker.value);
-        // when user doesn't give the special time we gonna show general 'forecast' for whole day
-        if (timePicker.value === '') {
-
-            weatherConditionInfo.textContent = jsonData.forecast.forecastday[0].day.condition.text;
-            weatherConditionLogo.src         = jsonData.forecast.forecastday[0].day.condition.icon;
-            temperatureDegree.textContent    = Math.round(jsonData.forecast.forecastday[0].day.avgtemp_c);
-            clockInfo.classList.add('hide');
-
-            // change the temp type
-            changeTemperature = () => 
-            changeTempType(jsonData.forecast.forecastday[0].day.avgtemp_f,jsonData.forecast.forecastday[0].day.avgtemp_c);
-            tempEL.addEventListener('click',changeTemperature);
-            tempELHasEvenListener = true;
-
-             /* show more details */
-            setWeatherDetails(jsonData.forecast.forecastday[0]);
-        } 
-        
-        // when user give a day from now and 5 days ahead with 'specific time'
-        else {
-          
-            weatherConditionInfo.textContent = jsonData.forecast.forecastday[0].hour[0].condition.text;
-            weatherConditionLogo.src         = jsonData.forecast.forecastday[0].hour[0].condition.icon;
-            clockInfo.textContent            = showRightTime(jsonData.forecast.forecastday[0].hour[0].time);
-            temperatureDegree.textContent    = Math.round(jsonData.forecast.forecastday[0].hour[0].feelslike_c);
-
-            // change the temp type
-            changeTemperature = () => 
-            changeTempType(jsonData.forecast.forecastday[0].hour[0].feelslike_f, jsonData.forecast.forecastday[0].hour[0].feelslike_c);
-            tempEL.addEventListener('click',changeTemperature);
-            tempELHasEvenListener = true;
-            
-             /* show more details */
-            setWeatherDetails(jsonData.forecast.forecastday[0].hour[0]);
-        }
-    }
-    
-}
-
-
-/* get data for Current Location */
-async function setCurrentLocationData(pos) {
-    const crd = pos.coords;
-    const endPoint = `https://api.weatherapi.com/v1/current.json?key=b458fd088f5b42c082691547210409&q=${crd.latitude},${crd.longitude}`;
-    const fetchData = await fetch(endPoint)
-    console.log(endPoint);
-    const data = await fetchData.json();
-
-    generalInfoSection.classList.remove('hide')
-    cityNameInfo.textContent = `${data.location.name} - ${data.location.region} - ${data.location.country}`;
-    temperatureType. textContent = 'C';
-
-    if (tempELHasEvenListener) {
-        tempEL.removeEventListener('click', changeTemperature);
-    }
-    
-    weatherConditionInfo.textContent = data.current.condition.text;
-    weatherConditionLogo.src         = data.current.condition.icon;
-    dayInfo.innerHTML                = showDayFormat(data.location.localtime);
-    clockInfo.textContent            = `${showRightTime(data.location.localtime)}`;
-    temperatureDegree.textContent    = Math.round(data.current.feelslike_c);
+  /* Show information in case of current weather data request */
+  if (searchMethod === "current") {
+    weatherConditionInfo.textContent = jsonData.current.condition.text;
+    weatherConditionLogo.src = jsonData.current.condition.icon;
+    dayInfo.innerHTML = showDayFormat(jsonData.location.localtime);
+    clockInfo.textContent = `${showRightTime(jsonData.location.localtime)}`;
+    temperatureDegree.textContent = Math.round(jsonData.current.feelslike_c);
 
     // change the temp type
-    changeTemperature = () => 
-    changeTempType(data.current.feelslike_f, data.current.feelslike_c);
-    tempEL.addEventListener('click',changeTemperature);
+    changeTemperature = () =>
+      changeTempType(
+        jsonData.current.feelslike_f,
+        jsonData.current.feelslike_c
+      );
+    tempEL.addEventListener("click", changeTemperature);
     tempELHasEvenListener = true;
 
     /* show more details */
-    setWeatherDetailsForCurrentLocation(data.current);
-    locationIcon.addEventListener('click', ()=> {
-        locationField.value = `${data.location.name}-${data.location.region}`
-    })
+    setWeatherDetails(jsonData.current);
+  } else if (searchMethod === "forecast" || searchMethod === "history") {
+
+  /* Show information in case of forecast weather data request */
+    dayInfo.innerHTML = showDayFormat(datePicker.value);
+    // when user doesn't give the special time we gonna show general 'forecast' for whole day
+    if (timePicker.value === "") {
+      weatherConditionInfo.textContent =
+        jsonData.forecast.forecastday[0].day.condition.text;
+      weatherConditionLogo.src =
+        jsonData.forecast.forecastday[0].day.condition.icon;
+      temperatureDegree.textContent = Math.round(
+        jsonData.forecast.forecastday[0].day.avgtemp_c
+      );
+      clockInfo.classList.add("hide");
+
+      // change the temp type
+      changeTemperature = () =>
+        changeTempType(
+          jsonData.forecast.forecastday[0].day.avgtemp_f,
+          jsonData.forecast.forecastday[0].day.avgtemp_c
+        );
+      tempEL.addEventListener("click", changeTemperature);
+      tempELHasEvenListener = true;
+
+      /* show more details */
+      setWeatherDetails(jsonData.forecast.forecastday[0]);
+    }
+
+    // when user give a day from now and 5 days ahead with 'specific time'
+    else {
+      weatherConditionInfo.textContent =
+        jsonData.forecast.forecastday[0].hour[0].condition.text;
+      weatherConditionLogo.src =
+        jsonData.forecast.forecastday[0].hour[0].condition.icon;
+      clockInfo.textContent = showRightTime(
+        jsonData.forecast.forecastday[0].hour[0].time
+      );
+      temperatureDegree.textContent = Math.round(
+        jsonData.forecast.forecastday[0].hour[0].feelslike_c
+      );
+
+      // change the temp type
+      changeTemperature = () =>
+        changeTempType(
+          jsonData.forecast.forecastday[0].hour[0].feelslike_f,
+          jsonData.forecast.forecastday[0].hour[0].feelslike_c
+        );
+      tempEL.addEventListener("click", changeTemperature);
+      tempELHasEvenListener = true;
+
+      /* show more details */
+      setWeatherDetails(jsonData.forecast.forecastday[0].hour[0]);
+    }
   }
-  
-  navigator.geolocation.getCurrentPosition(setCurrentLocationData);
+}
+
+/* get data for Current Location */
+async function setCurrentLocationData(pos) {
+  const crd = pos.coords;
+  const endPoint = `https://api.weatherapi.com/v1/current.json?key=b458fd088f5b42c082691547210409&q=${crd.latitude},${crd.longitude}`;
+  const fetchData = await fetch(endPoint);
+  const data = await fetchData.json();
+
+  generalInfoSection.classList.remove("hide");
+  cityNameInfo.textContent = `${data.location.name} - ${data.location.region} - ${data.location.country}`;
+  temperatureType.textContent = "C";
+
+  if (tempELHasEvenListener) {
+    tempEL.removeEventListener("click", changeTemperature);
+  }
+
+  weatherConditionInfo.textContent = data.current.condition.text;
+  weatherConditionLogo.src = data.current.condition.icon;
+  dayInfo.innerHTML = showDayFormat(data.location.localtime);
+  clockInfo.textContent = `${showRightTime(data.location.localtime)}`;
+  temperatureDegree.textContent = Math.round(data.current.feelslike_c);
+
+  // change the temp type
+  changeTemperature = () =>
+    changeTempType(data.current.feelslike_f, data.current.feelslike_c);
+  tempEL.addEventListener("click", changeTemperature);
+  tempELHasEvenListener = true;
+
+  /* show more details */
+  setWeatherDetailsForCurrentLocation(data.current);
+  locationIcon.addEventListener("click", () => {
+    locationField.value = `${data.location.name}-${data.location.region}`;
+  });
+}
+
+navigator.geolocation.getCurrentPosition(setCurrentLocationData);
